e5d84321f28338d176f658a401410617982e0817
Kelsey Monson <kelsey.monson@mssm.edu>
2025-04-28 17:22:29 -0400

    Create Getting_Started_with_R_script.R

---
+++ R-Project/Getting_Started_with_R_script.R
@@ -0,0 +1,16 @@
+# Getting Started with R Script
+# Follow along with this .R script for the codes necessary for today's exercise in the "Getting Started with R" class in the Umbrella Academy.
+
+# Install Packages ----
+# First, we install the packages we will need
+install.packages("tidyverse")
+install.packages("viridis")
+install.packages("readxl")
+
+# Load Libraries ----
+# Every R script should include some kind of header to describe the contents of the file, followed by loading of all the packages that will be used in the script
+
+# I also think it's a good idea to include a little note next to each package so you remember what you used it for:
+library(tidyverse)  # This is a bundle of many packages for data manipulation and plotting, including the essential "ggplot2" for making plots
+library(viridis)    # Color palettes designed to improve graph readability for readers with common forms of color blindness and/or color vision deficiency 
+library(readxl)     # A package improving on Base-R's ability to read in datasets, especially those saved as Excel spreadsheets
