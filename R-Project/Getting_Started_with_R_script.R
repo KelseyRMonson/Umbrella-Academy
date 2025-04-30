1593219afb053a8e1e472ac860ce1d2e7b290cbd
Kelsey Monson <kelsey.monson@mssm.edu>
2025-04-30 14:13:38 -0400

    Add str() to lesson

--- R-Project/Getting_Started_with_R_script.R
+++ R-Project/Getting_Started_with_R_script.R
@@ -13,12 +13,16 @@ install.packages("readxl")
 
 # I also think it's a good idea to include a little note next to each package so you remember what you used it for:
 library(tidyverse)  # This is a bundle of many packages for data manipulation and plotting, including the essential "ggplot2" for making plots
-library(viridis)    # Color palettes designed to improve graph readability for readers with common forms of color blindness and/or color vision deficiency 
+library(viridis)    # Color palettes designed to improve graph readability for readers with common forms of color blindness (they also just look nice) 
 library(readxl)     # A package improving on Base-R's ability to read in datasets, especially those saved as Excel spreadsheets
 
 # Load the Data ----
-# Download the dataset from this path: "Umbrella-Academy/R-Project/input/small_star_wars.xlsx"
-# Then create a folder in your own project directory called "input" and save the file there.
-
-# Now you can load it in
+# Follow the instructions to download and save the dataset
+# Then load it into R:
 small_star_wars <- read_excel("input/small_star_wars.xlsx", na="NA")
+
+# Explore the Data ----
+# Look at the structure
+# *Hint* Remember when we learned about "tab to complete" last week? 
+# Try it here now with the "small_star_wars" variable name -- start typing "sma" and hit TAB.
+str(small_star_wars)
